14225af0b2c9d4c98654a520b30737cf923626a5
sangmok0507 <baesangmok0507@gmail.com>
2026-08-18 00:54:46 +0900

    [level 2] Title: 테이블 해시 함수, Time: 11.08 ms, Memory: 163 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        
+        Arrays.sort(data, (a, b) -> { // 정렬 과정
+            if (a[col - 1] == b[col - 1])
+                return b[0] - a[0];
+            else
+                return a[col - 1] - b[col - 1];
+        });
+        
+        int answer = 0; 
+        for (int i = row_begin - 1; i < row_end; i++){ // bitwise XOR 연산 과정
+            int sum = 0;
+            for (int e : data[i])
+                sum += e % (i + 1);
+            answer = answer ^ sum;
+        }
+        return answer;
+    }
+}
